906494ad03bb0710f505baaf988eecd378bc2614
Srinivas Ganapathyraju <sganapathyraju@gmail.com>
2026-02-22 11:14:54 -0500

    Add ValueError for non-string input

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,8 @@
 """
 Validates strings as palindromes.
+Assignment #4
+Srinivas Ganapathyraju
 """
+def is_palindrome(value: str) -> bool:
+    if not isinstance(value, str):
+        raise ValueError("Input must be a string")
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,11 @@
 """
 Tests the palindrome module
+Assignment #4
+Srinivas Ganapathyraju
 """
+import pytest
+from palindrome import is_palindrome
+
+def test_raises_value_error_when_not_string():
+    with pytest.raises(ValueError):
+        is_palindrome(123)
